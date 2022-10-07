6d4c2bcc9759b4cd5013d8d16877aa890aed27a3
cathygarciapinon <cathygarciapinon@gmail.com>
2022-10-07 16:41:55 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,19 @@
+  /*----- constants -----*/
+const words = []; //word library
+
+
+  /*----- state variables -----*/
+let attempts; // the amount parts (6)
+let player;
+let bored; //each reindeer will disapear after each wrong answer
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+  initialize();
+
