172376d53eab8c5eead86fbd2536d3f583dcbb91
cathygarciapinon <cathygarciapinon@gmail.com>
2022-10-10 16:38:28 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -17,36 +17,39 @@ const maxWrong = 6;
 // const AUDIO = new Audio('')
 
   /*----- state variables -----*/
-let randomWord; 
-let answer;
-let wrongGuesses;
-let playerGuess;
-
+let randomWord; // array of characters for random words
+let wrongGuesses; // array of characters that are incorrect
+let guess; //current guessed letters
+let gameStatus; //Null(game in progress), W (win), L (lose)
 
   /*----- stored/ cached elements  -----*/
-const attemptsEl = document.getElementsById('attempts');
-const playerGuess = document.getElementById('playerGuess');
-const board = document.querySelectorAll('button');
-const message = document.querySelector('h3');
-console.log(keys[0.innerText])
+const msgEl = document.getElementsByClassName('message');
+const playBtn = document.getElementById('play');
+const livesLeft = document.getElementsByClassName('attempts');
+const letterBtn = [...document.querySelectorAll('article > button')]
+const secretEl = document.getElementsByClassName('secretWord');
+
+
 
   /*----- event listeners -----*/
-// resetPlayAgainBtn.addEventListener('click', initialize);
+playBtn.addEventListener('click', initialize);
 
 
 
   /*----- functions -----*/
-// initialize();
+initialize();
 
-// function initialize() {
-//     const wordList 
-// }
+function initialize() {
+    wrongGuesses = [];
+    const randomIdx = Math.floor(Math.random() * wordsList.length);
+    randomWord = wordsList[randomIdx].split('');
+    guess = randomWord.map(ltr => ltr === ' ' ? ' ' : '_');
+    gameStatus = null;
+    render();
+}
+
+function render() {
+  secretEl.textContent = guess.join('')
+}
 
  
-
-
-
-
-  // function renderMessage () {
-  //   if ()
-  // }
