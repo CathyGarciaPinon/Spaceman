0e44b8f496d6225900fd1896dba94f59af9caee9
cathygarciapinon <cathygarciapinon@gmail.com>
2022-10-11 12:58:06 -0700

    JS updates

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ let wrongGuesses; // array of characters that are incorrect
 let guess; //current guessed letters
 let gameStatus; //Null(game in progress), W (win), L (lose)
 
+
   /*----- stored/ cached elements  -----*/
 const msgEl = document.querySelector('.message');
 const playBtn = document.getElementById('play');
@@ -33,7 +34,7 @@ const secretEl = document.querySelector('.secretWord');
 
   /*----- event listeners -----*/
 playBtn.addEventListener('click', initialize);
-
+document.querySelector('article').addEventListener('click', handleChoice);
 
 
   /*----- functions -----*/
@@ -49,7 +50,63 @@ function initialize() {
 }
 
 function render() {
+  renderMessage()
+  // imageEl.src = `${IMAGES[wrongGuesses.length]}`;
   secretEl.textContent = guess.join('')
+  renderBtn()
 }
 
+function renderMessage() {
+  if (gameStatus === 'W') {
+      msgEl.textContent = `Yay, you did it!`;
+  } else if (gameStatus === 'L') {
+      msgEl.textContent = `Oh no, the word is ${randomWord.join('')}`;
+  } else {
+      msgEl.textContent = `${maxWrong - wrongGuesses.length + 1} Wrong guesses remain, Good luck!`
+  }
+}
+function renderBtn() {
+  letterBtn.forEach(function(btn){
+    const ltr = btn.textContent;
+    //if wrong guesses includes our letter add class name of wrong
+    if (wrongGuesses.includes(ltr)) {
+      btn.className = 'wrong';
+    } else if (guess.includes(ltr)) {
+      btn.className = 'correct';
+    } else {
+      btn.className = '';
+    }
+  })
+  playBtn.style.visibility = gameStatus ? 'visible' : 'hidden';
+}
+
+function handleChoice(event) {
+  const ltr = event.target.textContent
+  console.log(ltr)
+  if (
+    gameStatus ||
+    !letterBtn.includes(event.target) ||
+    wrongGuesses.includes(ltr) ||
+    guess.includes(ltr)  
+  ) return;
+
+  if (randomWord.includes(ltr)) {
+    //correct guess
+    randomWord.forEach(function(char,idx) {
+      if (char === ltr) guess[idx] = ltr 
+    });
+  } else {
+    wrongGuesses.push(ltr)
+    
+  }
+  gameStatus = getGameStatus()
+  render();
+}  
+
+function getGameStatus() {
+  if (!guess.includes('_')) return 'W';
+  //if wrong guesses.length is > maxWrongGuesses return 'L'
+  if (wrongGuesses.length > maxWrong) return 'L';
+  return null;
+}
  
